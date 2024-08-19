ba7f2c38ef174941edb58c378d6c2f39fbc523b9
FxFurEN <nikitostimoshenko@gmail.com>
2024-08-19 04:21:22 +0300

    add friends api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { hash } from 'bcrypt';
 import { CreateUserDto } from 'src/dto//user.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -19,6 +23,7 @@ export class UserService {
     const newUser = await this.prisma.user.create({
       data: {
         ...dto,
+        name: dto.name.startsWith('@') ? dto.name : `@${dto.name}`,
         password: await hash(dto.password, 10),
       },
     });
@@ -50,5 +55,83 @@ export class UserService {
     return this.prisma.cases.findMany({
       where: { authorId: userId },
     });
+  }
+
+  async addFriend(userId: string, friendName: string) {
+    if (!friendName.startsWith('@')) {
+      throw new ConflictException('Friend name must start with "@"');
+    }
+
+    const friend = await this.prisma.user.findFirst({
+      where: { name: friendName },
+    });
+
+    if (!friend) {
+      throw new NotFoundException('Friend not found');
+    }
+
+    if (userId === friend.id) {
+      throw new ConflictException('Cannot add yourself as a friend');
+    }
+
+    const existingFriendship = await this.prisma.friend.findFirst({
+      where: {
+        userId,
+        friendId: friend.id,
+      },
+    });
+
+    if (existingFriendship) {
+      throw new ConflictException('Friendship already exists');
+    }
+
+    return this.prisma.friend.create({
+      data: {
+        userId,
+        friendId: friend.id,
+      },
+    });
+  }
+
+  async removeFriend(userId: string, friendId: string) {
+    const friendship = await this.prisma.friend.findFirst({
+      where: {
+        userId,
+        friendId,
+      },
+    });
+
+    if (!friendship) {
+      throw new NotFoundException('Friendship not found');
+    }
+
+    return this.prisma.friend.delete({
+      where: {
+        id: friendship.id,
+      },
+    });
+  }
+
+  async getFriendIds(userId: string) {
+    const friends = await this.prisma.friend.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+
+    return friends.map((friend) => friend.friendId);
+  }
+
+  async getFriends(userId: string) {
+    const friendIds = await this.getFriendIds(userId);
+    const friends = await this.prisma.user.findMany({
+      where: {
+        id: {
+          in: friendIds,
+        },
+      },
+    });
+
+    return friends;
   }
 }
